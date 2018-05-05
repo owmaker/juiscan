a462557b044cee7a878bd2b51e033db07fa744eb
owmaker <38765232+owmaker@users.noreply.github.com>
2018-05-05 16:40:16 +0300

    first test commit

---
+++ src/juiscan/Application.java
@@ -0,0 +1,14 @@
+package juiscan;
+
+/**
+ * JUISCAN - Java uninquiring scan utility for Windows.
+ * @author Vorontsov D.S.
+ */
+public class Application {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
