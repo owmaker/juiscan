93e60460e26ea7f1794916768f5438e7c516f92c
owmaker <38765232+owmaker@users.noreply.github.com>
2018-05-05 21:15:44 +0300

    another test

--- src/juiscan/Application.java
+++ src/juiscan/Application.java
@@ -1,5 +1,11 @@
 package juiscan;
 
+import java.awt.EventQueue;
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.regex.Matcher;
+
 /**
  * JUISCAN - Java uninquiring scan utility for Windows.
  * @author Vorontsov D.S.
@@ -14,10 +20,38 @@ public class Application {
 	public static final String app_mail_hyperlink = "38765232+owmaker@users.noreply.github.com";
 	public static final String app_development_company = "";
 	public static final String[] app_autors = {"Vorontsov D.S."};
+
+	private static String CD;
+	private static String TmpDir;
 	
 	public static void main(String[] args) {
-
-
+		
+		try {
+			String dir = Class.forName("juiscan.Application").getProtectionDomain().getCodeSource().getLocation().getPath();
+			dir = URLDecoder.decode(dir, "UTF-8");
+			CD = dir.substring(0, dir.lastIndexOf("/")+1);
+			CD = CD.replaceAll("[/\\\\]+", Matcher.quoteReplacement(File.separator));
+			TmpDir = System.getProperty("java.io.tmpdir");
+			TmpDir = TmpDir.replaceAll("[/\\\\]+", Matcher.quoteReplacement(File.separator));
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			//Common.showErrorMessageDialog(e);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			//Common.showErrorMessageDialog(e);
+		}
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ProcessBarFrame pbf = new ProcessBarFrame();
+					pbf.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
 	}
 
 }
---
+++ src/juiscan/ProcessBarFrame.java
@@ -0,0 +1,56 @@
+package juiscan;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.SpringLayout;
+import javax.swing.border.EmptyBorder;
+
+public class ProcessBarFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	
+	public ProcessBarFrame() {
+		setType(Type.UTILITY);
+		setResizable(false);
+		//setUndecorated(true);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		int height = 150;
+		int width = 350;
+		
+		Dimension screen_dimensions = Toolkit.getDefaultToolkit().getScreenSize();
+		int screen_width = (int) screen_dimensions.getWidth();
+		int screen_height = (int) screen_dimensions.getHeight();
+		setBounds(screen_width - width, screen_height - height - 75, width, height);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		SpringLayout sl_contentPane = new SpringLayout();
+		contentPane.setLayout(sl_contentPane);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		sl_contentPane.putConstraint(SpringLayout.NORTH, lblNewLabel, 3, SpringLayout.NORTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.WEST, lblNewLabel, 6, SpringLayout.WEST, contentPane);
+		contentPane.add(lblNewLabel);
+		
+		JProgressBar progressBar = new JProgressBar();
+		sl_contentPane.putConstraint(SpringLayout.NORTH, progressBar, 6, SpringLayout.SOUTH, lblNewLabel);
+		sl_contentPane.putConstraint(SpringLayout.WEST, progressBar, 6, SpringLayout.WEST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, progressBar, -6, SpringLayout.EAST, contentPane);
+		progressBar.setValue(50);
+		contentPane.add(progressBar);
+		
+		JButton btnNewButton = new JButton("New button");
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnNewButton, -3, SpringLayout.SOUTH, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnNewButton, -6, SpringLayout.EAST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, progressBar, -6, SpringLayout.NORTH, btnNewButton);
+		contentPane.add(btnNewButton);
+	}
+}
