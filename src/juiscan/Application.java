1572de11ef2de1981f9289e9f2bae6697c53a4c8
owmaker <38765232+owmaker@users.noreply.github.com>
2018-05-05 20:12:51 +0300

    Application.java

--- src/juiscan/Application.java
+++ src/juiscan/Application.java
@@ -6,6 +6,15 @@ package juiscan;
  */
 public class Application {
 
+	public static final String app_name = "JUISCAN";
+	public static final String app_description = "JUISCAN - Java uninquiring scan utility for Windows";
+	public static final String app_version = "0.1";
+	public static final String app_build_timestamp = "2018.05.05";
+	public static final String app_development_dates = "2018";
+	public static final String app_mail_hyperlink = "38765232+owmaker@users.noreply.github.com";
+	public static final String app_development_company = "";
+	public static final String[] app_autors = {"Vorontsov D.S."};
+	
 	public static void main(String[] args) {
 
 
